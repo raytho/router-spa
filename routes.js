fc990b3bf07de5ab54f8c063f0a746a60a313d03
Raymundo Solis <pepperray01@gmail.com>
2020-09-22 11:34:52 -0500

    Router funcionando

---
+++ routes.js
@@ -0,0 +1,14 @@
+const routes = [
+  {
+    path: "/",
+    template: "<h1>hola</h1>",
+  },
+  {
+    path: "/contacto",
+    template: "<h1>Contacto</h1>",
+  },
+  {
+    path: "/about",
+    template: "<h1>About me</h1>",
+  },
+];
